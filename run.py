e4cf9b83186864692a7d9c89fa5716cb9f79d19c
김상훈 <kimsh94@fasol.co.kr>
2025-03-04 16:34:56 +0900

    Update run.py

--- run.py
+++ run.py
@@ -3,5 +3,5 @@ from app.main import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    # app.run(debug=True, host="127.0.0.1", port=8012)
-    app.run(debug=True, host="192.168.6.59", port=8012)
+    app.run(debug=True, host="0.0.0.0", port=8012)
+    # app.run(debug=True, host="192.168.6.59", port=8012)
